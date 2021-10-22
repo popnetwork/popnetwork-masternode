4155ba9cc1fd5b32d29f2c857caf6c389959e7d8
Alex Zhang <topdev.star1128@gmail.com>
2021-10-22 17:20:26 +0800

    Fix the max_balance

--- src/renderer/components/custom-button.js
+++ src/renderer/components/custom-button.js
@@ -4,7 +4,7 @@ module.exports = class CustomButton extends React.Component {
   render () {
     const { label, onClick, style, className, img, hoverImg, disabled = false } = this.props
     return (
-      <div className={`custom-button ${className}`} onClick={!disabled && onClick} style={disabled ? { ...style, opacity: 0.2 } : style}>
+      <div className={`custom-button ${className}`} onClick={!disabled ? onClick : undefined} style={disabled ? { ...style, opacity: 0.2 } : style}>
         {img && <img src={img} draggable={false} className={`${hoverImg ? 'normal' : null}`} />}
         {hoverImg && <img src={hoverImg} className="hover" draggable={false} />}
         <span>{label}</span>

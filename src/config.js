df4bc729b84b76a79f02070557448a9681182795
Alex Zhang <topdev.star1128@gmail.com>
2021-11-11 02:07:54 +0800

    Implements PM-83 and PM-86

--- src/config.js
+++ src/config.js
@@ -57,6 +57,7 @@ module.exports = {
     GITHUB_URL_ISSUES: 'https://github.com/popnetwork/popnetwork-masternode/issues',
     GITHUB_URL_RAW: 'https://raw.githubusercontent.com/popnetwork/popnetwork-masternode/master',
     GITHUB_URL_RELEASES: 'https://github.com/popnetwork/popnetwork-masternode/releases',
+    QUESTION_URL: 'https://help.thepopnetwork.org/',
     API_BASE_URL: 'https://masternode.popnet.work/api/v1',
     DEV_API_BASE_URL: 'https://dev.popnet.work/api/v1/',
 
